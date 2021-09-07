622054b5b002c8695b0ee669fcbb9564800621df
AleksandarBisevac <alek.bisevac@gmail.com>
2021-09-07 20:39:50 +0200

    imported bootstrap to index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-
-import './index.css';
+// CSS imports
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
