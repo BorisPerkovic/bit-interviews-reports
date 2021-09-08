45ca4eb9b0e5f2050ffa5a513d70cb22bc751b46
TanjaI <tanjuska611@gmail.com>
2021-09-08 21:29:26 +0200

    F-3 completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import { Fragment } from 'react';
 import LoginForm from './Components/LoginForm/LoginForm';
+import { Switch,Route } from 'react-router-dom';
+import Footer from './Components/Footer/Footer';
+import Header from './Components/Header/Header';
+import MainPage from './Components/MainPage/MainPage';
+
 
 const App = () => {
+  
   return (
     <Fragment>
-      <LoginForm />
+    <Header/>
+    <Switch>
+     <Route  exact path='/interviewreports' component={MainPage}/>
+     <Route        path='/login' component={LoginForm}/>
+    </Switch>
+    <Footer />
     </Fragment>
+
   );
 }
 
---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import classes from './Footer.module.css'
+
+
+const Footer=()=>{
+
+return(
+
+    <footer >
+        <p className='text-center'>Footer</p>
+    </footer>
+
+)
+
+
+
+}
+
+export default Footer ;
