69a5d288dba11c82a0f7f1f74182e42725262909
MarinaTP <you@example.com>
2021-09-08 12:45:50 +0200

    adding useState for password and email

--- src/Components/LoginForm/LoginForm.jsx
+++ src/Components/LoginForm/LoginForm.jsx
@@ -1,23 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import './LoginForm.module.css';
 
 const LoginForm = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const emailHandler = (event) => {
+        setEmail(event.target.value);
+        console.log(email);
+    }
+
+    const passwordHandler = (event) => {
+        setPassword(event.target.value);
+        console.log(password);
+    }
+
     return(
         <div className = 'container'>
             <div className = 'row'>
+                <h1 className = "text-center">Login</h1>
             <form>
                 <div className="mb-3">
                     <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" onChange = {emailHandler} />
                     <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
                 </div>
                 <div className="mb-3">
                     <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-                    <input type="password" className="form-control" id="exampleInputPassword1" />
-                </div>
-                <div className="mb-3 form-check">
-                    <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-                    <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
+                    <input type="password" className="form-control" id="exampleInputPassword1" onChange = {passwordHandler} />
                 </div>
                 <button type="submit" className="btn btn-primary">Submit</button>
             </form>
