df7f85340e189725be13f0850a08d160ae1be433
BorisPerkovic <borisperkovic89@yahoo.com>
2021-09-08 18:34:27 +0200

    form valdiation complete, access token take and set in LocalStorage

--- src/Components/LoginForm/LoginForm.jsx
+++ src/Components/LoginForm/LoginForm.jsx
@@ -1,58 +1,91 @@
-import React, { useState } from "react";
-import './LoginForm.module.css';
+import React, { useState, useRef } from "react";
+import Spinner from "../Spinner/Spinner";
+
+
+import classes from "./LoginForm.module.css";
 
 const LoginForm = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [form, setForm] = useState(false);
 
-    const emailHandler = (event) => {
-        setEmail(event.target.value);
-        console.log(email);
-    }
+    const [formValidaiton, setFormValidaiton] = useState({
+        email : true,
+        password: true
+    });
+    const [errorMessage, setErrorMessage] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
 
-    const passwordHandler = (event) => {
-        setPassword(event.target.value);
-        console.log(password);
-    }
+    const emailInputRef = useRef();
+    const passwordInputRef = useRef();
     
-    const validEmail = email.trim() !== '' && email.includes('@');
-    const validPassword = password.trim();
-
     const onSubmitHandler = (event) => {
         event.preventDefault();
-            if(!validEmail){
-                setForm(false)
-                    return;
+        setErrorMessage("");
+        const enteredEmail = emailInputRef.current.value;
+        const enteredPassword = passwordInputRef.current.value;
+
+        const emailValid = enteredEmail.trim() !== "" && enteredEmail.includes("@");
+        const passwordValid = enteredPassword.trim() !== "";
+
+        setFormValidaiton({
+            email: emailValid,
+            password: passwordValid
+        });
+
+        const formIsValid = emailValid && passwordValid;
+
+        if(!formIsValid) {
+            return;
+        }
+
+        const requestOptions = {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                email: enteredEmail,
+                password: enteredPassword
+            })
+        };
+
+        const logInRequest = async () => {
+            setIsLoading(true);
+            
+            const response = await fetch("http://localhost:3333/login", requestOptions);
+            const data = await response.json();
+            if(data === "Cannot find user" || data === "Incorrect password") {
+                setIsLoading(false);
+                setErrorMessage("Email or password are not correct!");
+            } else {
+                setIsLoading(false);
+                localStorage.setItem("token", data.accessToken);
             }
-            if(!validPassword){
-                setForm(false)
-                    return;
-            }
-        setForm(true);
-        console.log(form);
-    }
+        }
+        logInRequest();
+        
+
+    };
 
     return(
-        <div className = 'container'>
-            <div className = 'row'>
-                <h1 className = "text-center">Login</h1>
-            <form onSubmit = {onSubmitHandler}>
-                <div className="mb-3">
-                    <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" onChange = {emailHandler} />
-                    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-                    <input type="password" className="form-control" id="exampleInputPassword1" onChange = {passwordHandler} />
-                </div>
-                <button type="submit" className="btn btn-primary">Submit</button>
-            </form>
-
+        <div className = "container">
+            <div className = {`row ${classes["row-form"]}`}>
+            <div className="col-md-5 p-5 border rounded shadow">
+                <h1 className = "text-center">Log In</h1>
+                <form onSubmit = {onSubmitHandler}>
+                    <div className={`my-5 ${!formValidaiton.email ? classes.invalid : ""}`}>
+                        <label htmlFor="exampleInputEmail1" className="form-label" >Email address</label>
+                        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" ref={emailInputRef} />
+                        {!formValidaiton.email && <div id="emailHelp" className="form-text">Please, provide valid email address</div>}
+                    </div>
+                    <div className={`my-5 ${!formValidaiton.password ? classes.invalid : ""}`}>
+                        <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+                        <input type="password" className="form-control" id="exampleInputPassword1" ref = {passwordInputRef} />
+                        {!formValidaiton.password && <div id="emailHelp" className="form-text">Please, provide valid password</div>}
+                    </div>
+                    {isLoading && <Spinner />}
+                    <div className="py-3"><p className="text-center text-danger">{errorMessage}</p></div>
+                    <button type="submit" className="btn btn-primary form-control mt-3">Submit</button>
+                </form>
             </div>
-
+            </div>
         </div>
-    )
+    );
 };
 export default LoginForm;
