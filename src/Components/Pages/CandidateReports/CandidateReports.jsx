52204060dc2fc84d44557145161b8c8639bc5934
TanjaI <tanjuska611@gmail.com>
2021-09-11 14:34:10 +0200

    F7-finished

--- src/Components/Pages/CandidateReports/CandidateReports.jsx
+++ src/Components/Pages/CandidateReports/CandidateReports.jsx
@@ -1,8 +1,56 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
 import classes from './CandidateReports.module.css';
+import { AuthService } from "../../../services/auth.service";
+import Spinner from "../../Spinner/Spinner";
 
 
-const CandidateReports=()=>{
+const CandidateReports=(props)=>{
+
+const [candidate, setCandidate] = useState({});
+const [isLoading, setIsLoading] = useState(true); 
+const [logIn, setLogIn] = useState(false);
+// const [singleCandidateID,setSingleCandidateID]=useState('');
+
+
+
+ /* creating instance for AuthServices */
+const auth = new AuthService();
+
+
+
+/* function for checking if user is Logged In, if user is not Logged In redirect user to LogIn page */
+const isLogedIn = () => {
+  const token = auth.getToken();
+  if (!token) {
+    window.location.assign("http://localhost:3000/login");
+    return false;
+  }
+  setLogIn(true);
+  // setSingleCandidateID(props.match.params.id);
+  return true;
+};
+
+const getCandidate = () => {
+  const singleCandidateID=props.match.params.id;
+  const fetchCandidates = async () => {
+    const candidatesResponse = await auth.getSingleCandidate(singleCandidateID); 
+    console.log(candidatesResponse);
+    setCandidate(candidatesResponse);
+    setIsLoading(false);
+  }
+  fetchCandidates();
+}
+
+
+/* creating useEffect on mounting CandidatesReports  component for isLogedIn and getCandidate functions */
+useEffect(isLogedIn, []);
+useEffect(getCandidate, []);
+
+// {isLoading && <Spinner />}
+  let date = new Date(candidate.birthday);
+  let month = date.toLocaleString("en-US", {month: "2-digit"});
+  let day = date.toLocaleString("en-US", {day: "2-digit"});
+  let year = date.getFullYear();
 
 return(
 
@@ -11,12 +59,12 @@ return(
             <div className = {`col ${classes.picture}`}><img className = "img-fluid" src = "https://www.pngitem.com/pimgs/m/30-307416_profile-icon-png-image-free-download-searchpng-employee.png"></img></div>
             <div className = "container col">
               <div className = "row h-50 py-2">
-                <div className = "col"><span>Name: </span><p className = "px-3">Marina</p></div>
-                <div className = "col"><span>Date of birth: </span><p className = "px-3">01.01.2001</p></div>
+                <div className = "col"><span>Name:</span><p className = "px-3">{candidate.name}</p></div>
+                <div className = "col"><span>Date of birth: </span><p className = "px-3">{month}.{day}.{year}</p></div>
               </div>
               <div className = "row h-50">
-                <div className = "col"><span>Email: </span><p className = "px-3">marina@gmail.com</p></div>
-                <div className = "col"><span>Education: </span><p className = "px-3">bitovac</p></div>
+                <div className = "col"><span>Email: </span><p className = "px-3">{candidate.email}</p></div>
+                <div className = "col"><span>Education: </span><p className = "px-3">{candidate.education}</p></div>
               </div>
             </div>
 
@@ -68,5 +116,6 @@ return(
 
 
 }
+
 
 export default CandidateReports;
