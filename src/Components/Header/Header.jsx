35237724d38afabb23baf5fbaa018a127663ffe2
TanjaI <tanjuska611@gmail.com>
2021-09-14 21:39:10 +0200

    F11-components created

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -13,6 +13,8 @@ const Header = () => {
             {path && (<header className='container d-flex justify-content-between align-items-center p-4'>
                 <h1>Interview Reports</h1>
                 <Link to="/"><button type='button' className='btn btn-info'>Candidates</button></Link> 
+                <Link to="/reports"><button type='button' className='btn btn-info'>Reports</button></Link> 
+                <Link to="/login"><button type='button' className='btn btn-info'>Log Out</button></Link> 
             </header>)}
         </Fragment>   
     );
---
+++ src/Components/Pages/Reports/ReportCard/ReportCard.jsx
@@ -0,0 +1,33 @@
+
+import React, { useState } from "react";
+import {formateDate} from '../../../../utils/date-function.js'
+
+
+   
+const ReportCard=({item})=>{
+
+
+    return (
+
+        
+        <div className='row my-4 p-3'>
+            <div className='col-sm-3 border-right border-dark'>{item.companyName}</div>
+            <div className='col-sm-3 border-right border-dark'>{item.candidateName}</div>
+            <div className='col-sm-2 border-right border-dark'>{formateDate(item.interviewDate)}</div>
+            <div className='col-sm-2 border-right border-dark'>{item.status}</div>
+            <div className='col-sm-2  '></div>
+
+        </div>
+        
+
+        ) 
+  
+      
+
+
+}
+
+
+ 
+
+export default ReportCard;
