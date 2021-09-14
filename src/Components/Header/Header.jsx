aba457e35f949b0934d8d03bba78b19f1876a447
AleksandarBisevac <alek.bisevac@gmail.com>
2021-09-14 21:45:13 +0200

    F11 - added logout function which removes token and redirect user to login page

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -2,23 +2,38 @@ import React, { Fragment } from "react";
 import { Link } from "react-router-dom";
 import { useHistory, useLocation } from "react-router-dom";
 
-
 const Header = () => {
+  const location = useLocation();
+  const path = location.pathname !== "/login";
 
-    const location = useLocation();
-    const path = location.pathname !== "/login";
+  const logOut = (e) => {
+    e.preventDefault();
+    localStorage.removeItem("token");
+    window.location.assign("/login");
+  };
 
-    return(
-        <Fragment>
-            {path && (<header className='container d-flex justify-content-between align-items-center p-4'>
-                <h1>Interview Reports</h1>
-                <Link to="/"><button type='button' className='btn btn-info'>Candidates</button></Link> 
-                <Link to="/reports"><button type='button' className='btn btn-info'>Reports</button></Link> 
-                <Link to="/login"><button type='button' className='btn btn-info'>Log Out</button></Link> 
-            </header>)}
-        </Fragment>   
-    );
-
+  return (
+    <Fragment>
+      {path && (
+        <header className="container d-flex justify-content-between align-items-center p-4">
+          <h1>Interview Reports</h1>
+          <Link to="/">
+            <button type="button" className="btn btn-info">
+              Candidates
+            </button>
+          </Link>
+          <Link to="/reports">
+            <button type="button" className="btn btn-info">
+              Reports
+            </button>
+          </Link>
+          <button onClick={logOut} type="button" className="btn btn-info">
+            Log Out
+          </button>
+        </header>
+      )}
+    </Fragment>
+  );
 };
 
 export default Header;
