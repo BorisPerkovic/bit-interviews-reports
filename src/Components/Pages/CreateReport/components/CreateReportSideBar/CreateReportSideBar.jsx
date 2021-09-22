fbeaf6b179ed870715b790e0e9b0d18a98eb948f
AleksandarBisevac <alek.bisevac@gmail.com>
2021-09-22 20:15:56 +0200

    hotfix-3 : solved conflicts

--- src/Components/Pages/CreateReport/components/CreateReportSideBar/CreateReportSideBar.jsx
+++ src/Components/Pages/CreateReport/components/CreateReportSideBar/CreateReportSideBar.jsx
@@ -1,10 +1,6 @@
 import React from "react";
-<<<<<<< HEAD:src/Components/Pages/CreateReport/components/CreateReportSideBar/CreateReportSideBar.jsx
 import { CREATE_REPORT_SIDE_BAR } from "../../../../../constants/endpoints";
-=======
-import { CREATE_REPORT_SIDE_BAR } from "../../../../../../constants/endpoints";
 import classes from "./CreateReportSideBar.module.css";
->>>>>>> development:src/Components/Pages/CandidateReports/CreateReport/components/CreateReportSideBar/CreateReportSideBar.jsx
 
 const CreateReportSideBar = ({ switchValue, candidate }) => {
   return (
@@ -25,27 +21,20 @@ const CreateReportSideBar = ({ switchValue, candidate }) => {
           );
         })}
       </ul>
-<<<<<<< HEAD:src/Components/Pages/CreateReport/components/CreateReportSideBar/CreateReportSideBar.jsx
-      <ul className="border-top border-dark mt-5 py-5">
-        {(switchValue === 2 || switchValue === 3) && (
-          <li className="my-4">
-            <span>Candidate</span> <h3>{candidate.candidateName}</h3>
-          </li>
-        )}
-        {switchValue === 3 && (
-          <li className="my-4">
-            <span>Company</span> <h3>{candidate.companyName}</h3>
-          </li>
-        )}
-      </ul>
-=======
       <div className={`mt-3 py-2 ${classes["reports-details"]}`}>
         <ul>
-          {(switchValue === 2 || switchValue === 3) && <li className="my-4"><span>Candidate</span> <h3>{candidate.candidateName}</h3></li>}
-          {switchValue === 3 && <li className="my-4"><span>Company</span> <h3>{candidate.companyName}</h3></li>}
+          {(switchValue === 2 || switchValue === 3) && (
+            <li className="my-4">
+              <span>Candidate</span> <h3>{candidate.candidateName}</h3>
+            </li>
+          )}
+          {switchValue === 3 && (
+            <li className="my-4">
+              <span>Company</span> <h3>{candidate.companyName}</h3>
+            </li>
+          )}
         </ul>
       </div>
->>>>>>> development:src/Components/Pages/CandidateReports/CreateReport/components/CreateReportSideBar/CreateReportSideBar.jsx
     </div>
   );
 };
